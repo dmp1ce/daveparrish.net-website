dce6b71b52fd73af9340a9d57bffa23e9cbd5bcc
David Parrish <daveparrish@tutanota.com>
2022-05-28 19:32:10 -0400

    Initial commit for DaveParrish.net website on GitHub pages

---
+++ README.md
@@ -0,0 +1,3 @@
+# DaveParrish.net generated site
+
+Generated files from [Hakyll static site generator](https://github.com/dmp1ce/daveparrish.net) for the [Dave Parrish website](https://daveparrish.net).
---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Helpful for syncing files from generated Hakyll site
+
+if [ -z "$1" ]; then
+    echo "Provide a Hakyll site directory to deploy from."
+    echo "Example: ./deploy ~/Workspace/daveparrish.net"
+    exit 1
+fi
+
+rsync -av "$1/_site/" docs/ --delete
